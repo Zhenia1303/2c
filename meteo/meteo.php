7d4d0e1b2e63e77d69120885c31c93402bfc558f
Zhenia1303 <koorama005@gmail.com>
2025-09-19 13:35:39 +0200

    Add files via upload

---
+++ meteo/meteo.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prognoza pogody Poznań</title>
+    <link rel="stylesheet" href="styl4.css">
+</head>
+
+<body>
+    <div class="gura">
+        <div class="gurny-lewy">
+            <p>maj, 2019 r.</p>
+        </div>
+        <div class="gurny-srodkowy">
+            <h2>Prognoza dla Poznania</h2>
+        </div>
+        <div class="gurny-prawy">
+            <img src="image/logo.png" alt="prognoza">
+        </div>
+    </div>
+    <div class="center">
+        <div class="lewy">
+            <a href="kwerendy.txt">Kwerendy</a>
+        </div>
+        <div class="prawy">
+            <img src="image/obraz.jpg" alt="Polska, Poznań">
+        </div>
+    </div>
+    <div class="gluwna">
+        <table>
+            <tr class="table-gura">
+                <th>Lp.</th>
+                <th>DATA</th>
+                <th>NOC - TEMPERATURA</th>
+                <th>DZIEŃ - TEMPERATURA</th>
+                <th>OPADY [mm/h]</th>
+                <th>CIŚNIENIE [hPa]</th>
+            </tr>
+
+            <tr>
+                <td>1</td>
+                <td>2019-05-18</td>
+                <td>12</td>
+                <td>15</td>
+                <td>30</td>
+                <td>996</td>
+            </tr>
+
+            <tr>
+                <td>2</td>
+                <td>2019-05-18</td>
+                <td>12</td>
+                <td>15</td>
+                <td>30</td>
+                <td>996</td>
+            </tr>
+
+            <tr>
+                <td>3</td>
+                <td>2019-05-18</td>
+                <td>12</td>
+                <td>15</td>
+                <td>30</td>
+                <td>996</td>
+            </tr>
+
+            <tr>
+                <td>4</td>
+                <td>2019-05-18</td>
+                <td>12</td>
+                <td>15</td>
+                <td>30</td>
+                <td>996</td>
+            </tr>
+
+            <tr>
+                <td>5</td>
+                <td>2019-05-18</td>
+                <td>12</td>
+                <td>15</td>
+                <td>30</td>
+                <td>996</td>
+            </tr>
+
+            <tr>
+                <td>6</td>
+                <td>2019-05-18</td>
+                <td>12</td>
+                <td>15</td>
+                <td>30</td>
+                <td>996</td>
+            </tr>
+        </table>
+    </div>
+    <div class="stopki">
+        <p>Stronę wykonał:aboba</p>
+    </div>
+
+</body>
+
+</html>
