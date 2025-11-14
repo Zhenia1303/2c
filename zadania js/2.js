adddfcd6ec39a6e103f9d0b8a92f218e031ea943
Zhenia1303 <koorama005@gmail.com>
2025-11-14 12:56:27 +0100

    Add files via upload

---
+++ zadania js/2.js
@@ -0,0 +1,19 @@
+let text = document.getElementById('text');
+
+function taro() {
+    let random = Math.floor(Math.random() * 100);
+    if (random <= 33) {
+        text.innerText = "Prawdopodobieństwo, że tak będzie wynosi:" + random + "%";
+        text.style.color = "red";
+    }   else if (random >= 34 && random <=66) {
+        text.innerText = "Prawdopodobieństwo, że tak będzie wynosi:" + random + "%";
+        text.style.color = "blue";
+    } else {
+        text.innerText = "Prawdopodobieństwo, że tak będzie wynosi:" + random + "%";
+        text.style.color = "green";
+    }
+}
+
+
+let bth = document.getElementById('bth');
+bth.addEventListener("click", taro);
