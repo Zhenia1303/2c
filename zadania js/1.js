0e7ddaafc22ea9718e8d57fa187c89578d432880
Zhenia1303 <koorama005@gmail.com>
2025-11-14 12:18:01 +0100

    Add files via upload

---
+++ zadania js/1.js
@@ -0,0 +1,18 @@
+let result = document.getElementById('result');
+
+function trojkat() {
+    let a = parseFloat(document.getElementById('a').value);
+    let b = parseFloat(document.getElementById('b').value);
+    let c = parseFloat(document.getElementById('c').value);
+
+    let p = (a + b + c) / 2;
+    let P = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    if ((a + b) > c && (a + c) > b && (b + c) > a) {
+        result.innerHTML = P;
+    } else {
+        result.innerText = " Z podanych boków nie sposób utworzyć trójkąta!"
+    }
+}
+
+    let bth = document.getElementById('bth');
+    bth.addEventListener("click", trojkat);
